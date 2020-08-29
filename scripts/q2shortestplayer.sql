bc225f47754b77efe515fcb526b4e1c22ba839ca
drewkonig <drewdat@gmail.com>
2020-08-29 13:14:52 -0500

    optimized q2

--- scripts/q2shortestplayer.sql
+++ scripts/q2shortestplayer.sql
@@ -1,8 +1,6 @@
 SELECT 	CONCAT(p.namefirst, ' ', p.namelast) AS name,
-		/* converts height to feet and inches, requires GROUP BY, very slow
-		(ROUND(CAST(MIN(height) AS int)/12,0)) AS feet, 
-		MOD(CAST(MIN(height) AS numeric),12) AS inches, */
-		p.height,
+		(SELECT (ROUND(CAST(MIN(height) AS int)/12,0)) FROM people as p) AS feet, 
+		(SELECT (MOD(CAST(MIN(height) AS numeric),12)) FROM people as p) AS inches,
 		t.name AS team, 
 		a.g_all AS games
 FROM people as p
@@ -10,6 +8,5 @@ INNER JOIN appearances as a
 ON p.playerid = a.playerid
 INNER JOIN teams as t
 ON a.teamid = t.teamid
---GROUP BY p.namefirst, p.namelast, t.name, a.g_all, p.height
 ORDER BY height
 LIMIT 1;
